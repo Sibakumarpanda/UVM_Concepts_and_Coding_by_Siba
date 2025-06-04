8e8747c78ac77a4580fd8f5339f9cb0289082bea
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2025-06-04 12:58:13 +0530

    Create 10_uvm_factory_overriding.sv - Committed Factory overriding in uvm

---
+++ 10_uvm_factory_overriding.sv
@@ -0,0 +1,4 @@
+Factory Overriding:
+Based on the requirement, the behavior of the testbench can be changed by substituting one class with another when it is constructed. 
+It is the process that allows an object of one type to be overridden with an object of its derived type without changing the testbench structure.   
+This facility of the uvm factory allows users to override the class without editing or recompiling the code.
