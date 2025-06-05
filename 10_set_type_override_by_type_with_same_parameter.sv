f862d821cd29e737d7b7b8f72f04917b15866e9f
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2025-06-05 08:57:57 +0530

    Update 10_set_type_override_by_type_with_same_parameter.sv - Updated

--- 10_set_type_override_by_type_with_same_parameter.sv
+++ 10_set_type_override_by_type_with_same_parameter.sv
@@ -1,2 +1,2 @@
-Example9: for set_type_override_by_type with parameterized classes
-Note: For parameterized classes, an overriding class should have the same parameter values.
+Example9: for set_type_override_by_type with parameterized classes (same parameter)
+Note: For parameterized classes, an overriding class should have the same parameter values, else it will fail.
