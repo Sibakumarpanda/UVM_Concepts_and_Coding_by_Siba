2f892125ba3f75fdabb77178e1b3f17291af86d1
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2025-06-07 07:28:34 +0530

    Update 12_uvm_objection_concept.sv - Updated objection concept

--- 12_uvm_objection_concept.sv
+++ 12_uvm_objection_concept.sv
@@ -1,1 +1,10 @@
+UVM Objection Concept:
 
+It provides a mechanism to coordinate status information between two or more components, objects. 
+The uvm_objection class is extended from uvm_report_object.
+The objection deals with the concept of raise and drop objection which means the internal counter is increment and decrement respectively. 
+Each participating component and object may raise or drop objections asynchronously. 
+When all objections are dropped, the counter value will become zero. 
+The objection has to be raised before starting any process and drop it once it is completed.
+Decleration :  
+class uvm_objection extends uvm_report_object  
