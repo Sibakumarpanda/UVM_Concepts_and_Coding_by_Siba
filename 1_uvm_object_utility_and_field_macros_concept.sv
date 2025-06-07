797e450ed28fa8ca66d90a19c2f75544a9f2a7c6
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2025-06-07 22:48:18 +0530

    Create 1_uvm_object_utility_and_field_macros_concept.sv - Added new file

---
+++ 1_uvm_object_utility_and_field_macros_concept.sv
@@ -0,0 +1,6 @@
+UVM Object:
+
+The uvm_object class is the base class for all uvm hierarchical classes such as uvm_report_object, uvm_component, uvm_transaction, uvm_sequence_item, uvm_sequence etc. 
+It serves an important role to define a set of methods such as create, copy, print, clone, compare, record, etc.
+
+UVM Utility Macros and field macros:
