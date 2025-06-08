c4584eea80d8146962179aa102e31cb08a1b7cb8
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2025-06-08 21:43:20 +0530

    Update 1_uvm_object_utility_and_field_macros_concept.sv - Updated

--- 1_uvm_object_utility_and_field_macros_concept.sv
+++ 1_uvm_object_utility_and_field_macros_concept.sv
@@ -10,3 +10,25 @@ UVM Utility Macros and field macros:
 -The `uvm_field_* macro accepts at least two arguments as arg (instance name of the variable that is compatible with `uvm_field_* macro) and flag (act as a control mechanism to add corresponding data methods).
 -The new() method is important to define with the corresponding class name as an augment.
 -Note: During compilation time, these UVM automation macros expand the complete code available for the corresponding macros.
+
+Syntax without field macros :    `uvm_object_utils(<class Type>)
+Syntax with field macros:        `uvm_object_utils_begin(<class_type>)
+                                      `uvm_field_*(<arg>, <flag>)
+                                 `uvm_object_utils_end
+-Below field macros are commonly used for data methods (copy, compare, pack, unpack, print, clone, etc) for scalar/dynamic class properties. The complete list can be studied from the UVM reference document.
+  
+`uvm_field_* macros : The `uvm_field_* macros implement data methods for scalar properties.
+
+    `uvm_field_* macros                   |                                               Description
+
+     `uvm_field_int                                                                       Implements data methods for any packed integral property.
+
+      `uvm_field_string                                                                   Implements data methods for a string property.
+
+      `uvm_field_real                                                                     Implements data methods for any real property.
+
+      `uvm_field_enum                                                                     Implements data methods for an enumerated property.
+
+      `uvm_field_event                                                                    Implements data methods for an event property.
+
+      `uvm_field_object                                                                   Implements data methods for an uvm_object based property.
