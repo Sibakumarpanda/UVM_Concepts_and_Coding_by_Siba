6a9181683891377a8fd06aa0fb5512c868bf9d73
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2025-06-11 10:58:05 +0530

    Update 13_resource_database_concept_in_uvm.sv - Updated

--- 13_resource_database_concept_in_uvm.sv
+++ 13_resource_database_concept_in_uvm.sv
@@ -1,1 +1,7 @@
+Resource database Concept and Mechanism in UVM :
 
+The UVM centralized resource database is used to store configurable objects, variables, virtual interfaces, queues, class handles, etc and retrieve them from the database. 
+Hence , such a configurable testbench provides a degree of freedom to the verification engineer to use provided information in various parts of the testbench.
+The resource database consists of polymorphic resource containers that store each resource in the form of a lookup table.
+A lookup table is nothing but a centralized resource database. 
+The uvm_resource_base and uvm_resource as shown below are uvm source code snippets.
