12f65432595692d97e71c0cc46949cdfc02a36b1
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2025-06-12 12:21:42 +0530

    Create 13_uvm_resource_db_class_and_methods.sv - updated and committed

---
+++ 13_uvm_resource_db_class_and_methods.sv
@@ -0,0 +1,6 @@
+uvm_resource_db in UVM:
+
+-The uvm_resource_db is a type-parameterized class that is a convenience layer on the top of a resource database. 
+-This convenience layer provides simplified access to the low-level database and it adds no new functionality. 
+-Hence, uvm_resource_db is not derived from the uvm_resource class. 
+-The below code snippet for the uvm_resource_db class is taken from the uvm source code.
