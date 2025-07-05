9755f37c08daf3017bb72ba6dc8e1574cb3be344
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2025-07-05 11:24:02 +0530

    Update 19_uvm_driver_understanding.sv - Updated the file

--- 19_uvm_driver_understanding.sv
+++ 19_uvm_driver_understanding.sv
@@ -8,7 +8,37 @@ UVM Driver understanding:
 -RSP sequence item is optional. 
 -Usually, the REQ and RSP sequence item has the same class type. They can be different if it is declared specifically.
 
-uvm_driver class declaration:
+uvm_driver class declaration:    class uvm_driver #( type REQ = uvm_sequence_item, type RSP = REQ ) extends uvm_component  
+
+How to write the driver code?
   
-class uvm_driver #( type REQ = uvm_sequence_item, type RSP = REQ ) extends uvm_component  
+-Create a user-defined driver class extended from uvm_driver and register it in the factory.
+-Declare virtual interface handle to retrieve actual interface handle using configuration database in the build_phase.
+-Write standard new() function. Since the driver is a uvm_component. The new() function has two arguments as string name and uvm_component parent.
+-Implement build_phase and get interface handle from the configuration database.
+-Implement run_phase to get the sequence items and drive them to the DUT using a virtual interface handle.  
+
+//UVM Driver Sample code Snippet :
+class driver extends uvm_driver#(seq_item);
+  virtual add_if vif;
+  `uvm_component_utils(driver)
+  
+  function new(string name = "driver", uvm_component parent = null);
+    super.new(name, parent);
+  endfunction
+  
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    if(!uvm_config_db#(virtual add_if) :: get(this, "", "vif", vif))
+      `uvm_fatal(get_type_name(), "Not set at top level");
+  endfunction
+  
+  task run_phase (uvm_phase phase);
+    // Get the sequence_item and drive it to DUT
+  endtask
+endclass
+  
+
+
+  
   
