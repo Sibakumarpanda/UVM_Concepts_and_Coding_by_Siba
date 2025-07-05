9b4c231c1bccad89dda0ffbd8381f1d330ac0ca5
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2025-07-05 12:18:01 +0530

    Update 20_uvm_sequence_understanding_basics.sv - Updated and committed

--- 20_uvm_sequence_understanding_basics.sv
+++ 20_uvm_sequence_understanding_basics.sv
@@ -1,1 +1,5 @@
 
+UVM Sequence understanding:
+-UVM sequence is a container that holds data items (uvm_sequence_items) which are sent to the driver via the sequencer.
+-Note: uvm_sequence class is parameterized with uvm_sequence_item.
+-uvm_sequence class declaration: virtual class uvm_seqence #( type REQ = uvm_sequence_item, type RSP = REQ) extends uvm_sequence_base
