91c0c4fcf804a6a799b88a5bc439ce552e740196
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2025-07-06 20:36:07 +0530

    Update 20_how_to_write_a_sequence.sv - Updated

--- 20_how_to_write_a_sequence.sv
+++ 20_how_to_write_a_sequence.sv
@@ -5,4 +5,7 @@ How to write a sequence ???
   2.Using existing methods from the base class
     a. Using wait_for_grant(), send_request(), wait_for_item_done() etc
     b. Using start_item/finish_item methods.
--Let’s discuss the macro-based approach in UVM sequence macro and existing methods approach in the uvm_sequence_base class methods section
+    
+-Let’s discuss the macro-based approach in UVM sequence macro and existing methods approach in the uvm_sequence_base class methods section in below files 
+  https://github.com/Sibakumarpanda/UVM_Concepts_and_Coding_by_Siba/blob/main/20_how_to_write_a_sequence_using_uvm_sequence_macro.sv
+  https://github.com/Sibakumarpanda/UVM_Concepts_and_Coding_by_Siba/blob/main/20_how_to_write_a_sequence_using_uvm_sequence_base_methods.sv
