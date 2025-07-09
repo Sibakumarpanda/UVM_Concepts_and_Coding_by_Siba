7a6818bbeb9bb285d193f351e94819eeb38e4e3b
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2025-07-09 08:02:14 +0530

    Update and rename 22_sequencer_arbitration_concept.sv to 22_Arbitration_in_sequencer_concept_understanding.sv- Updated

--- 22_Arbitration_in_sequencer_concept_understanding.sv
+++ 22_Arbitration_in_sequencer_concept_understanding.sv
@@ -1,3 +1,3 @@
-Arbitration in Sequencer:
+Arbitration in Sequencer Understanding:
 -The uvm_sequencer has a built-in mechanism to arbitrate within concurrently running sequences over the sequencer. 
 -Based on the arbitration algorithm, the sequencer sends sequence_item to the driver for the granted sequence.
