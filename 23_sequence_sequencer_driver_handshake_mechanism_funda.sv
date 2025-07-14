7eece148f55c6b0040e748f91e9e60e3f645c862
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2025-07-14 07:38:12 +0530

    Create 23_sequence_sequencer_driver_handshake_mechanism_funda.sv - Updated

---
+++ 23_sequence_sequencer_driver_handshake_mechanism_funda.sv
@@ -0,0 +1,4 @@
+Sequence-Driver-Sequencer communication in UVM :
+-We discussed sequece_item, sequence, sequencer, and driver independently. 
+-In this section, we will discuss how they talk with each other and provide sequence items from sequence to driver via the sequencer. 
+-Before you start reading this section, make sure you are aware of all methods used in sequencer and driver. (Refer: UVM Sequencer and UVM Driver).
