17cfc7f2a35620e66cb56adee280d4053a60ad0b
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2025-07-15 17:33:45 +0530

    Update 25_uvm_agent_understanding.sv - updated and committed

--- 25_uvm_agent_understanding.sv
+++ 25_uvm_agent_understanding.sv
@@ -20,10 +20,9 @@ Types of Agent :
 
 How to configure the agent as an active or passive agent???
 
--There are three ways we can do the above configuration. they are
+-There are two ways we can do the above configuration. they are
    1. configure agent type with set_config_int
    2. configure agent type with uvm_config_db
-   3. Configuring user-defined agent to an active or passive agent
        
  1. configure agent type with set_config_int:   
 -An agent is usually instantiated at UVM environment class. 
@@ -39,11 +38,59 @@ How to configure the agent as an active or passive agent???
 2. configure agent type with uvm_config_db  
      Link:
 
-3. Configuring user-defined agent to an active or passive agent
-   -The get_is_active() function is used to find out the type of agent. 
-   -The driver, sequencer instances are created if it is an active agent and monitor instance can be created by default irrespective of agent type.
-   -The get_is_active() function returns an enum as UVM_ACTIVE or UVM_PASSIVE of type uvm_active_passive_enum.
-   - For better understanding , an example is created in below link ,
-     Link:
+How does the user-defined agent know whether it is an active or passive agent?
+
+-The get_is_active() function is used to find out the type of agent. 
+-The driver, sequencer instances are created if it is an active agent and monitor instance can be created by default irrespective of agent type.
+-The get_is_active() function returns an enum as UVM_ACTIVE or UVM_PASSIVE of type uvm_active_passive_enum.
+- For better understanding , the below code will be helpful .
+   
+//Active agent Sample code
+class a_agent extends uvm_agent;
+  driver drv;
+  sequencer seqr;
+  monitor_A mon_A;
+  `uvm_component_utils(a_agent)
+  
+  function new(string name = "a_agent", uvm_component parent = null);
+    super.new(name, parent);
+  endfunction
+  
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    if(get_is_active() == UVM_ACTIVE) begin
+      drv = driver::type_id::create("drv", this);
+      seqr = sequencer::type_id::create("seqr", this);
+      `uvm_info(get_name(), "This is Active agent", UVM_LOW);
+    end
+    mon_A = monitor_A::type_id::create("mon_A", this);
+  endfunction
+  
+  function void connect_phase(uvm_phase phase);
+    super.connect_phase(phase);
+    if(get_is_active() == UVM_ACTIVE)
+      drv.seq_item_port.connect(seqr.seq_item_export);
+  endfunction
+endclass :a_agent
+
+//Passive agent code
+class p_agent extends uvm_agent;
+  monitor_B mon_B;
+  `uvm_component_utils(p_agent)
+  
+  function new(string name = "p_agent", uvm_component parent = null);
+    super.new(name, parent);
+  endfunction
+  
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    if(get_is_active() == UVM_PASSIVE) begin
+      mon_B = monitor_B::type_id::create("mon_B", this);
+      `uvm_info(get_name(), "This is Passive agent", UVM_LOW);
+    end
+  endfunction
+endclass :p_agent  
+   
+     
    
    
