1081d29a57d0c5f07a148cddb2281cb091fb089e
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2025-07-15 11:29:30 +0530

    Update 24_uvm_monitor_understanding.sv -Updated and committed

--- 24_uvm_monitor_understanding.sv
+++ 24_uvm_monitor_understanding.sv
@@ -9,5 +9,48 @@ UVM Monitor Understanding:
 
 Purpose of Monitor:
 -Capture signal level information and translate it into the transaction.
--Broadcast transactions to other components using TLM port for coverage collection and checking purposes (scoreboard). It can also control them with enable/ disable knobs if required.
+-Broadcast transactions to other components using TLM port for coverage collection and checking purposes (scoreboard).
+-It can also control them with enable/ disable knobs if required.
 -Capture protocol specific information and forward it to the related scoreboard or checker where protocol check happens . 
+
+How to create a UVM monitor???
+  
+-Create a user-defined monitor class extended from uvm_monitor and register it in the factory.
+-Declare virtual interface handle to retrieve actual interface handle using configuration database in the build_phase.
+-Declare analysis port to broadcast the sequence items or transactions.
+-Write standard new() function. Since the monitor is a uvm_component. The new() function has two arguments as string name and uvm_component parent.
+-Implement build_phase and get interface handle from the configuration database.
+-Implement run_phase to sample DUT interface using a virtual interface handle and translate into transactions. 
+-The write() method sends transactions to the collector component. 
+
+//UVM_Monitor code Snippet
+ class my_monitor extends uvm_monitor;
+  // declaration for the virtual interface, analysis port, and monitor sequence item.
+   seq_item mon_item;
+   virtual add_if vif;
+   
+  uvm_analysis_port #(seq_item) item_collect_port;
+  `uvm_component_utils(my_monitor)
+  
+  // constructor
+  function new(string name = "my_monitor", uvm_component parent = null);
+    super.new(name, parent);
+    item_collect_port = new("item_collect_port", this);
+    mon_item = new();
+  endfunction
+  
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    if(!uvm_config_db#(virtual add_if) :: get(this, "", "vif", vif))
+      `uvm_fatal(get_type_name(), "Not set at top level");
+  endfunction
+  
+  task run_phase (uvm_phase phase);
+    forever begin
+      // Sample DUT information and translate into transaction
+      item_collect_port.write(mon_item);
+    end
+  endtask
+endclass :my_monitor
+
+ 
