0c147c272273e7f19d77d0d8a1210a58bb0cf136
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2025-07-16 10:56:13 +0530

    Update 26_uvm_scoreboard_understanding.sv - Updated and committed

--- 26_uvm_scoreboard_understanding.sv
+++ 26_uvm_scoreboard_understanding.sv
@@ -20,4 +20,96 @@ How to write scoreboard code in UVM???
 -Implement run_phase to check DUT functionality throughout simulation time.
 
 //Scoreboard Sample code Snippet:
+class my_scoreboard extends uvm_scoreboard;
+  `uvm_component_utils(my_scoreboard)
   
+  uvm_analysis_imp #(seq_item, my_scoreboard) item_collect_export;
+  seq_item item_q[$];
+   
+  function new(string name = "my_scoreboard", uvm_component parent = null);
+    super.new(name, parent);
+    item_collect_export = new("item_collect_export", this);
+  endfunction
+  
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+  endfunction
+  
+  function void write(seq_item req);
+    `uvm_info(get_type_name, $sformatf("Received transaction = %s", req), UVM_LOW);
+    item_q.push_back(req);
+  endfunction
+  
+  task run_phase (uvm_phase phase);
+    seq_item sb_item;
+    forever begin
+      wait(item_q.size > 0);
+      
+      if(item_q.size > 0) begin
+        sb_item = item_q.pop_front();
+        // Checking comparing logic
+        ...        
+      end
+    end
+  endtask
+  
+endclass :my_scoreboard 
+
+UVM Scoreboad types :
+-Depends on design functionality scoreboards can be implemented in two ways.
+   1.In-order scoreboard
+   2.Out-of-order scoreboard  
+  
+1. In-order scoreboard :
+-The in-order scoreboard is useful for the design whose output order is the same as driven stimuli.
+-The comparator will compare the expected and actual output streams in the same order. 
+-They will arrive independently. Hence, the evaluation must block until both expected and actual transactions are present.  
+-To implement such scoreboards, an easier way would be to implement TLM analysis FIFOs. 
+-For more details visit the TLM analysis FIFO section. 
+-In the below example, there are two monitors whose analysis port is connected to the scoreboard to provide input and output transactions.
+// Sample code Snippet for Inorder Scoreboard
+  
+class inorder_sb extends uvm_scoreboard;
+  `uvm_component_utils(inorder_sb)
+  
+  uvm_analysis_export #(txn) in_export, out_export;
+  uvm_tlm_analysis_fifo #(txn) in_fifo, out_fifo;
+
+  function new (string name = "inorder_sb" , uvm_component parent = null) ;
+    super.new(name, parent);
+  endfunction
+
+  function void build_phase (uvm_phase phase);
+    in_fifo    = new("in_fifo", this);
+    out_fifo   = new("out_fifo", this);
+    in_export  = new("in_export", this);
+    out_export = new("out_export", this);
+  endfunction
+
+  function void connect_phase (uvm_phase phase);
+    in_export.connect(in_fifo.analysis_export);
+    out_export.connect(out_fifo.analysis_export);
+  endfunction
+
+  task run_phase( uvm_phase phase);
+    txn in_txn;
+    txn exp_txn, act_txn;
+    forever begin
+      in_fifo.get(in_txn);
+      process_data(in_txn, exp_txn); 
+      out_fifo.get(act_txn);
+      if (!exp_txn.compare(act_txn)) begin
+        `uvm_error(get_full_name(), $sformat("%s does not match %s", exp_txn.sprint(), act_txn.sprint()), UVM_LOW);
+      end
+    end
+  endtask
+  
+  // Reference model 
+  task process_data(input txn in_txn, output txn exp_txn);
+    // Generate expected txn for driven stimulus
+    ...
+    ...
+  endtask
+endclass :inorder_sb
+  
+2. Out-of-order scoreboard :  
