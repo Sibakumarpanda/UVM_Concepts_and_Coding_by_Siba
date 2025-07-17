928692e249ed808554cf9c628d4f4651c9709186
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2025-07-17 12:04:54 +0530

    Create 29_uvm_printer_concept_understanding.sv - Updated and committed

---
+++ 29_uvm_printer_concept_understanding.sv
@@ -0,0 +1,8 @@
+UVM Printer Understanding:
+-The uvm_printer class provides flexibility to print uvm_objects in different formats. 
+-We have discussed the print() method using `uvm_field_* macro or write do_print() method if utils_begin/ end macros are not used.
+-The UVM printer provides four built-in printers.
+  1.uvm_printer
+  2.uvm_table_printer
+  3.uvm_tree_printer
+  4.uvm_line_printer
