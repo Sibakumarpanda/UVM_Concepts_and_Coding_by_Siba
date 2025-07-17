99774c4eb295a3b32ac970149e198e28fb4b2370
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2025-07-17 09:37:56 +0530

    Update 28_uvm_comparer_understanding.sv -Updated and committed

--- 28_uvm_comparer_understanding.sv
+++ 28_uvm_comparer_understanding.sv
@@ -1,1 +1,12 @@
+UVM Comparer Understanding:
+-In uvm_object, we discussed print, clone, copy, compare methods, etc. 
+-The compare() method compares two objects to return 1 in case of successful comparison. 
+-The uvm_comparer adds up policy for the comparison and counts the number of miscompares if any.
 
+Variables in uvm_comparer class:
+  
+Type                     Variables                                 Description
+  
+enum                     policy                                    It is an enum variable that compares the type UVM_DEEP, UVM_SHALLOW, or UVM_REFERENCE.
+int unsigned             show_max  
+  
