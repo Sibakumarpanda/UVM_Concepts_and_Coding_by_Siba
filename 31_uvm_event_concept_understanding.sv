dc9e4e7de5fa29453eafc35fb17f8df515df73eb
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2025-07-22 20:14:39 +0530

    Update 31_uvm_event_concept_understanding.sv -updated and committed

--- 31_uvm_event_concept_understanding.sv
+++ 31_uvm_event_concept_understanding.sv
@@ -11,7 +11,7 @@ UVM Event Understanding:
 -The events provide synchronization between processes by triggering an event from one process and waiting for that event in another process to be triggered. 
 -The UVM event can also pass data when it is triggered that makes it different from the SV event.
      
-uvm_event_base class method                               
+uvm_event_base class method :                               
   
 Type                Methods                         Description
 
@@ -20,3 +20,25 @@ function            new                            Creates a new object for the 
 task                wait_on                        Waits for the event to be activated for the first time until the event is reset. This task returns immediately if the event has already been triggered.  
 
 task                wait_off                       For an already triggered event, this task waits for an event to be turned ‘off’ via a call to reset function.  
+
+task                wait_trigger                   Waits for the event to be triggered   
+
+task                wait_ptrigger                  Waits for a persistent trigger of the event. The trigger views as persistent within a given time slice to avoid a race condition. 
+
+function            is_on                          Returns 1 if an event has triggered.  
+
+function            is_off                         Returns 1 if an event has not been triggered.
+
+function            get_trigger_time               To get time at which event was last triggered. The time reruns 0 if an event has been reset or not triggered.
+
+function            reset                          Resets event to its off state. No callbacks are called during reset.  
+
+function            get_num_waiters                Returns the number of processes waiting on that event.   
+
+function            cancel                         Decrement the number of waiters at the event. 
+
+uvm_event class methods :
+   
+Type               Methods                         Description 
+
+function           new                             Creates a new object for the event   
