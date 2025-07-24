ed03e4a01cc3f90e14e910be2e255e71db851734
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2025-07-24 12:08:55 +0530

    Update 32_uvm_barrier_concept_understanding.sv -updated and committed

--- 32_uvm_barrier_concept_understanding.sv
+++ 32_uvm_barrier_concept_understanding.sv
@@ -9,3 +9,28 @@ UVM Barrier Usage:
 -Once the threshold is reached, the processes are unblocked by the wait_for method.  
 
 UVM Barrier Methods:  
+
+Type                      Methods                           Description
+
+function                  new                               Creates a new object for barrier  
+
+function                 set_threshold                      Sets a process threshold that waits till the mentioned number of processes reaches the barrier. 
+                                                            Once the threshold is reached, all waiting processes are activated.  
+  
+function                 get_threshold                      Gets current threshold value for the barrier.
+
+task                     wait_for                           Waits for the number of processes being set by the set_threshold method before continuing from the barrier. 
+
+function                 get_num_waiters                    Returns the number of processes currently waiting at the barrier.  
+
+function                 reset                              Resets the barrier and sets the waiter process count to zero. 
+                                                            After reset, the barrier will force processes to wait for the same threshold value again. 
+                                                            The reset function does not change the threshold value which was set by the set_threshold method last time .
+
+function                 set_auto_reset                     Once the threshold is reached, the barrier should reset itself. 
+                                                            This is the default configuration that makes sure blocking of new processes until the threshold reaches again.
+                                                            If set_auto_reset is turned off, once the threshold is reached, 
+                                                            new processes will not be blocked until the barrier is reset.   
+
+function                 cancel                             Decrements the waiter count by one that is used when a process is being killed or activated 
+                                                            while waiting on the barrier.                                                              
