15e0c1ae0d070d6d9aadb7107747bcc4b7f10813
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2025-07-24 09:48:27 +0530

    Update 31_uvm_event_various_posible_example.sv -Updated and committed

--- 31_uvm_event_various_posible_example.sv
+++ 31_uvm_event_various_posible_example.sv
@@ -637,14 +637,161 @@ module event_example();
 endmodule
 	
 //LogFile Output Using Cadence Xcelium Tool
+[2025-07-24 04:14:00 UTC] xrun -Q -unbuffered '-timescale' '1ns/1ns' '-sysv' '-access' '+rw' -uvmnocdnsextra -uvmhome $UVM_HOME $UVM_HOME/src/uvm_macros.svh design.sv testbench.sv  
+TOOL:	xrun	23.09-s001: Started on Jul 24, 2025 at 00:14:00 EDT
+xrun: 23.09-s001: (c) Copyright 1995-2023 Cadence Design Systems, Inc.
+    transaction tr_A = new();
+                     |
+xmvlog: *W,VARIST (testbench.sv,22|21): Local static variable with initializer requires 'static' keyword.
+	Top level design units:
+		uvm_pkg
+		$unit_0x4ccdf83b
+		event_example
+Loading snapshot worklib.event_example:sv .................... Done
+SVSEED default: 1
+xcelium> source /xcelium23.09/tools/xcelium/files/xmsimrc
+xcelium> source /xcelium23.09/tools//methodology/UVM/CDNS-1.2/sv/files/tcl/uvm_sim.tcl
+xcelium> run
+UVM_INFO /xcelium23.09/tools/methodology/UVM/CDNS-1.2/sv/src/base/uvm_root.svh(412) @ 0: reporter [UVM/RELNOTES] 
+----------------------------------------------------------------
+CDNS-UVM-1.2 (23.09-s001)
+(C) 2007-2014 Mentor Graphics Corporation
+(C) 2007-2014 Cadence Design Systems, Inc.
+(C) 2006-2014 Synopsys, Inc.
+(C) 2011-2013 Cypress Semiconductor Corp.
+(C) 2013-2014 NVIDIA Corporation
+----------------------------------------------------------------
 
+  ***********       IMPORTANT RELEASE NOTES         ************
 
+  You are using a version of the UVM library that has been compiled
+  with `UVM_NO_DEPRECATED undefined.
+  See http://www.eda.org/svdb/view.php?id=3313 for more details.
+
+  You are using a version of the UVM library that has been compiled
+  with `UVM_OBJECT_DO_NOT_NEED_CONSTRUCTOR undefined.
+  See http://www.eda.org/svdb/view.php?id=3770 for more details.
+
+      (Specify +UVM_NO_RELNOTES to turn off this notice)
+
+@0: waiting for the event e1
+@10: Before triggering event e1
+-------------------------------------
+Name         Type         Size  Value
+-------------------------------------
+transaction  transaction  -     @1798
+  addr       integral     8     'h2f 
+  data       integral     8     'hb3 
+-------------------------------------
+@10: After triggering event e1
+@10: event e1 is triggered and data received = 
+-------------------------------------
+Name         Type         Size  Value
+-------------------------------------
+transaction  transaction  -     @1798
+  addr       integral     8     'h2f 
+  data       integral     8     'hb3 
+-------------------------------------
+
+xmsim: *W,RNQUIE: Simulation is complete.
+xcelium> exit
+TOOL:	xrun	23.09-s001: Exiting on Jul 24, 2025 at 00:14:06 EDT  (total: 00:00:06)
+Done
 	
 Example9: Type C3: retrieve data using wait_ptrigger_data 
-
 	
+module event_example();
+  uvm_event e1;
+  
+  task process_A();
+    transaction tr_A = new();
+    #10;
+    $display("@%0t: Before triggering event e1", $time);
+    assert(tr_A.randomize);
+    tr_A.print();
+    e1.trigger(tr_A);
+    $display("@%0t: After triggering event e1", $time);
+  endtask
+  
+  task process_B();
+    uvm_object event_data;
+    transaction tr_B;
+    
+    $display("@%0t: waiting for the event e1", $time);
+    e1.wait_ptrigger_data(event_data);
+    $cast(tr_B, event_data);
+    $display("@%0t: event e1 is triggered and data received = \n%s", $time, tr_B.sprint());
+  endtask
+
+  initial begin
+    e1 = new();
+    fork
+      process_A();
+      process_B();
+    join
+  end
+endmodule	
 //LogFile Output Using Cadence Xcelium Tool
 
-	
+[2025-07-24 04:16:53 UTC] xrun -Q -unbuffered '-timescale' '1ns/1ns' '-sysv' '-access' '+rw' -uvmnocdnsextra -uvmhome $UVM_HOME $UVM_HOME/src/uvm_macros.svh design.sv testbench.sv  
+TOOL:	xrun	23.09-s001: Started on Jul 24, 2025 at 00:16:54 EDT
+xrun: 23.09-s001: (c) Copyright 1995-2023 Cadence Design Systems, Inc.
+    transaction tr_A = new();
+                     |
+xmvlog: *W,VARIST (testbench.sv,22|21): Local static variable with initializer requires 'static' keyword.
+	Top level design units:
+		uvm_pkg
+		$unit_0x4ccdf83b
+		event_example
+Loading snapshot worklib.event_example:sv .................... Done
+SVSEED default: 1
+xcelium> source /xcelium23.09/tools/xcelium/files/xmsimrc
+xcelium> source /xcelium23.09/tools//methodology/UVM/CDNS-1.2/sv/files/tcl/uvm_sim.tcl
+xcelium> run
+UVM_INFO /xcelium23.09/tools/methodology/UVM/CDNS-1.2/sv/src/base/uvm_root.svh(412) @ 0: reporter [UVM/RELNOTES] 
+----------------------------------------------------------------
+CDNS-UVM-1.2 (23.09-s001)
+(C) 2007-2014 Mentor Graphics Corporation
+(C) 2007-2014 Cadence Design Systems, Inc.
+(C) 2006-2014 Synopsys, Inc.
+(C) 2011-2013 Cypress Semiconductor Corp.
+(C) 2013-2014 NVIDIA Corporation
+----------------------------------------------------------------
+
+  ***********       IMPORTANT RELEASE NOTES         ************
+
+  You are using a version of the UVM library that has been compiled
+  with `UVM_NO_DEPRECATED undefined.
+  See http://www.eda.org/svdb/view.php?id=3313 for more details.
+
+  You are using a version of the UVM library that has been compiled
+  with `UVM_OBJECT_DO_NOT_NEED_CONSTRUCTOR undefined.
+  See http://www.eda.org/svdb/view.php?id=3770 for more details.
+
+      (Specify +UVM_NO_RELNOTES to turn off this notice)
+
+@0: waiting for the event e1
+@10: Before triggering event e1
+-------------------------------------
+Name         Type         Size  Value
+-------------------------------------
+transaction  transaction  -     @1798
+  addr       integral     8     'h2f 
+  data       integral     8     'hb3 
+-------------------------------------
+@10: After triggering event e1
+@10: event e1 is triggered and data received = 
+-------------------------------------
+Name         Type         Size  Value
+-------------------------------------
+transaction  transaction  -     @1798
+  addr       integral     8     'h2f 
+  data       integral     8     'hb3 
+-------------------------------------
+
+xmsim: *W,RNQUIE: Simulation is complete.
+xcelium> exit
+TOOL:	xrun	23.09-s001: Exiting on Jul 24, 2025 at 00:16:59 EDT  (total: 00:00:05)
+Done	
 	
 	
