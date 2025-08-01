cef955baf269cd30fefbf547e42a8b255b590b6b
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2025-08-01 11:07:13 +0530

    Update 35_uvm_queue_concept_understanding.sv - Updated and committed

--- 35_uvm_queue_concept_understanding.sv
+++ 35_uvm_queue_concept_understanding.sv
@@ -1,1 +1,10 @@
+UVM Queue Concept Understanding:
+-The uvm_queue class builds a dynamic queue that to be allotted on-demand basis and passed by reference.
+-uvm_queue class declaration:   class uvm_queue #( type T = int ) extends uvm_object
 
+uvm_queue class Methods :
+
+  Methods                                                           Description
+
+  function new ( string name = “” )                                Creates a new queue for the given name  
+  
