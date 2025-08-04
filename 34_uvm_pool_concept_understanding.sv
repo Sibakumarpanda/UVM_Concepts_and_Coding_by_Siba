15f55defd38bde303b87e89a0cb635db3745f545
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2025-08-04 14:04:07 +0530

    Update 34_uvm_pool_concept_understanding.sv -Updated and committed

--- 34_uvm_pool_concept_understanding.sv
+++ 34_uvm_pool_concept_understanding.sv
@@ -1,1 +1,14 @@
+UVM Pool Concept Understanding:
 
+-The uvm_pool is a parameterized class that implements a pool data structure same as System Verilog associative array and it can be allocated based on demand. 
+-The items in the pool can be shared across different verification components as pass by reference. 
+-On copying the uvm_pool object, it copies the class handle instead of copying an entire associative array.
+-uvm_pool class declaration:  class uvm_pool # (type KEY = int, T = uvm_void) extends uvm_object
+
+uvm_pool class Methods :
+  
+  Methods                                                            Description
+
+  function new ( string name = “” )                                  Creates pool for the given name 
+
+  static function T get_global ( KEY key )                          Returns the specified item instance from the global item pool.  
