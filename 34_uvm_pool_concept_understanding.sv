a74e601de318e6454c83481602e52a5a0b01ce2d
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2025-08-04 15:32:30 +0530

    Update 34_uvm_pool_concept_understanding.sv -updated and committed

--- 34_uvm_pool_concept_understanding.sv
+++ 34_uvm_pool_concept_understanding.sv
@@ -15,7 +15,32 @@ uvm_pool class Methods :
 
   static function this_type get_global_pool ()                     Returns the singleton global pool for the item of type T.  
 
-  virtual function int num ()                                    Returns the total number of items in the pool for unique keys 
+  virtual function int num ()                                     Returns the total number of items in the pool for unique keys 
 
   virtual function void add ( KEY key, T item )                  Adds given item for the given key as a (key, item) pair. 
                                                                  If an item exists already for the given key, the previous item is overwritten with the new item.  
+
+  virtual function T get ( KEY key )      
+                                                                     Returns the item for the given key. 
+                                                                     If no item exists by that key, it creates a new item and returns the same for that key.  
+
+  virtual function void delete ( KEY key ) Removes the item from the pool for the given key.
+
+  virtual function int exists ( KEY key ) Returns 1 if an item exists in the pool for the given key otherwise, it returns 0.
+
+  virtual function int next ( ref KEY key ) If the next key is found, then the function returns 1, and the key is updated with that key.
+
+                                            If the input key is the last key in the pool, then the function returns 0, and the key is left unchanged.  
+
+  virtual function int prev ( ref KEY key )- If the previous key is found, then the function returns 1, and the key is updated with that key.
+
+                                               If the input key is the first key in the pool, then the function returns 0, and the key is left unchanged
+
+  virtual function int first ( ref KEY key )- Returns the key of the first item as a key stored in the pool if the pool is not empty and 1 is returned.
+                                                The key is unchanged if the pool is empty and 0 is returned.
+
+  virtual function int last ( ref KEY key )-Returns the key of the last item as a key stored in the pool if the pool is not empty and 1 is returned
+
+                                            The key is unchanged if the pool is empty and 0 is returned                                                
+
+                                                                       
