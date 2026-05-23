c1eaf9dbadf8cba0c73eae62a015b264527d403f
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-05-23 17:30:03 +0530

    Refactor UVM class hierarchy documentation
    Updated formatting and clarified descriptions for UVM class hierarchy.

--- 8_5_uvm_class_hierarchy_imp_points.sv
+++ 8_5_uvm_class_hierarchy_imp_points.sv
@@ -1,18 +1,23 @@
-UVM classes have 3 major types :
+//////////////////////////////////////////
+  UVM classes have 3 major types
+///////////////////////////////////////////
 
 UVM object
 UVM transaction
 UVM component
 
 uvm_void : 
-The uvm_void is the base class for all the UVM classes. It is an abstract class that does not have any data members or methods implemented. It is a generic container for objects to be created similar to void pointer in C language.
+The uvm_void is the base class for all the UVM classes. It is an abstract class that does not have any data members or methods implemented. 
+It is a generic container for objects to be created similar to void pointer in C language.
 Syntax: virtual class uvm_void
 
 uvm_object: 
-The UVM object is a data structure used for testbench configuration and it is the base class available for component and sequence branch. The uvm_object provides methods like create, clone, copy, record, compare, print, etc.
+The UVM object is a data structure used for testbench configuration and it is the base class available for component and sequence branch. The uvm_object provides methods like create, clone, copy, 
+record, compare, print, etc.
 
 uvm_report_object: 
-The uvm_report_object provides reporting functionality for UVM. The message, error, or warning prints are very important for debugging purposes that are being facilitated by the uvm_report_object class.
+The uvm_report_object provides reporting functionality for UVM. 
+The message, error, or warning prints are very important for debugging purposes that are being facilitated by the uvm_report_object class.
 Most of the methods in uvm_report_object are passed to an instance of uvm_report_handler that stores reporting configuration. 
 Based on the configurations, uvm_report_handler decides whether to display the message or not. 
 It is then passed to uvm_report_server for actual formatting and producing the messages. 
@@ -109,4 +114,5 @@ These are hardcoded arguments for the constructor and the user is not allowed to
 Difference between uvm_object, uvm_transaction and uvm_sequence_item:
 The uvm_transaction class is inherited from uvm_object that adds additional information of a timing, notification events, and recording interface.
 The uvm_sequence_item class is derived from the uvm_transaction class that adds basic functionality for sequence and sequence items like get_sequence_id, set_sequencer, get_sequence, etc.
-It is important to note that uvm_transaction usage is deprecated as a base class for user-defined transactions. Hence, the uvm_sequence_item class shall be used as a base class for user-defined transactions.
+It is important to note that uvm_transaction usage is deprecated as a base class for user-defined transactions. 
+Hence, the uvm_sequence_item class shall be used as a base class for user-defined transactions.
