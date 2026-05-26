78c8781dbb66c170c315d8bc14c26e54c1083ca5
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-05-26 18:43:53 +0530

    Update 20_3_how_to_write_a_sequence.sv

--- 20_3_how_to_write_a_sequence.sv
+++ 20_3_how_to_write_a_sequence.sv
@@ -1,4 +1,6 @@
-How to write a sequence ???
+////////////////////////////////////
+   How to write a sequence ???
+/////////////////////////////////////     
 -An intention is to create a seq_item, randomize it and then send it to the driver. 
 -To perform this operation any one of the following approaches is followed in the sequence.
   1.Using macros like `uvm_do , `uvm_create, `uvm_send etc
@@ -7,5 +9,5 @@ How to write a sequence ???
     b. Using start_item/finish_item methods.
     
 -Let’s discuss the macro-based approach in UVM sequence macro and existing methods approach in the uvm_sequence_base class methods section in below files 
-  https://github.com/Sibakumarpanda/UVM_Concepts_and_Coding_by_Siba/blob/main/20_how_to_write_a_sequence_using_uvm_sequence_macro.sv
-  https://github.com/Sibakumarpanda/UVM_Concepts_and_Coding_by_Siba/blob/main/20_how_to_write_a_sequence_using_uvm_sequence_base_methods.sv
+    https://github.com/Sibakumarpanda/UVM_Concepts_and_Coding_by_Siba/blob/main/20_how_to_write_a_sequence_using_uvm_sequence_macro.sv
+    https://github.com/Sibakumarpanda/UVM_Concepts_and_Coding_by_Siba/blob/main/20_how_to_write_a_sequence_using_uvm_sequence_base_methods.sv
