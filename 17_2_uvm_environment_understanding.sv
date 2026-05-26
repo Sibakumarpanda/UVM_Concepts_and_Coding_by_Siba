d1cc9213d162bb769543d0decfd599ce76360fb1
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-05-26 16:50:22 +0530

    Update 17_2_uvm_environment_understanding.sv

--- 17_2_uvm_environment_understanding.sv
+++ 17_2_uvm_environment_understanding.sv
@@ -1,39 +1,22 @@
-UVM Environment Understanding:
--An environment provides a well-mannered hierarchy and container for agents, scoreboards and other verification components .
+////////////////////////////////////////
+  UVM Environment Understanding
+////////////////////////////////////////
+-An environment provides a well-mannered hierarchy and container for agents, scoreboard and other verification components like functional coverage block.
 -The other verif component includes other environment classes that are helpful in reusing block-level environment components at the SoC level. 
 -The user-defined env class has to be extended from the uvm_env class.
+-Class Declaration :                    virtual class uvm_env extends uvm_component;
+-User-defined env class declaration :   class <env_name> extends uvm_env;
 
-Class Declaration :                    virtual class uvm_env extends uvm_component;
-User-defined env class declaration :   class <env_name> extends uvm_env;
-  
-Steps for how to create a UVM env?
-  
+//////////////////////////////////////////  
+  Steps to create a UVM env???
+//////////////////////////////////////////    
 -Create a user-defined env class extended from uvm_env and register it in the factory.
 -In the build_phase, instantiate the agent, other verification components and use the configuration database to set/get configuration variables.
 -In the connect_phase, connect the monitor, scoreboard and other functional coverage components using the TLM interface.  
 
-
-UVM Environment
-An environment provides a well-mannered hierarchy and container for agents, scoreboards, and other verification components including other environment classes that are helpful in reusing block-level environment components at the SoC level. The user-defined env class has to be extended from the uvm_env class.
-
-UVM Environment block diagram
-uvm_env class hierarchy
-uvm_env hierarchy
-Class Declaration
-
-virtual class uvm_env extends uvm_component;
-User-defined env class declaration
-
-class <env_name> extends uvm_env;
-How to create a UVM env?
-Steps:
-
-Create a user-defined env class extended from uvm_env and register it in the factory.
-In the build_phase, instantiate the agent, other verification components, and use the configuration database to set/get configuration variables.
-In the connect_phase, connect the monitor, scoreboard, and other functional coverage components using the TLM interface.
-  
-// UVM Environment Sample example :
-  
+////////////////////////////////////  
+   UVM Environment Sample example 
+///////////////////////////////////  
 class my_env extends uvm_env;
   `uvm_component_utils(my_env)
   agent agt;
@@ -53,13 +36,12 @@ class my_env extends uvm_env;
   
   function void connect_phase(uvm_phase phase);
     // connect agent and scoreboard using TLM interface    
-    // Ex. agt.mon.item_collect_port.connect(sb.item_collect_export);
+    // Example:  agt.mon.item_collect_port.connect(sb.item_collect_export);
   endfunction
   
 endclass :my_env
 
-//How to instantiate multiple env in top_env:
-  
+//How to instantiate multiple env in top_env 
 class top_env extends uvm_env;
   my_env env_o[3];
   `uvm_component_utils(top_env)
