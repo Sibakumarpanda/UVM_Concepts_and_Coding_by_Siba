8dc953d4e0c13cdde0eb8c424839c9f9b0610158
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-05-26 07:58:55 +0530

    Revise UVM test understanding documentation
    Updated documentation for UVM test understanding, including steps to write a test case and execution details.

--- 16_2_uvm_test_understanding.sv
+++ 16_2_uvm_test_understanding.sv
@@ -1,23 +1,28 @@
-
-UVM Test Understanding:
+//////////////////////////////////
+   UVM Test Understanding
+/////////////////////////////////
 -The test is at the top of the hierarchical component that initiates the environment component construction. 
 -It is also responsible for the testbench configuration and stimulus generation process. 
 -Based on the features and functionalities of the design mentioned in the verification plan, tests are written. 
 -The user-defined test class is derived from uvm_test.
 -Class Declarations:  virtual class uvm_test extends uvm_component
 -The test includes environment class, configurations, and start sequences over sequencers. 
--It is recommended to have a base test that includes all testbench settings including an instance of the environment class, create configurations, etc and varieties of tests as per the verification test plan.
--This can be extended from the base test to avoid repeating the same code in every test. The derived tests can also set configurations based on test requirements.  
-
-Steps to write a test case:
+-It is recommended to have a base test that includes all testbench settings including an instance of the environment class, create configurations, etc
+ and varieties of tests as per the verification test plan.
+-This can be extended from the base test to avoid repeating the same code in every test. The derived tests can also set configurations based on test requirements.
+  
+///////////////////////////////
+   Steps to write a test case
+//////////////////////////////  
 -Create a user-defined test class extended from uvm_test and register it in the factory.
 -Declare environment, sequence handle, and configuration objects based on the requirement.
 -Write standard new() function. Since the test is a uvm_component. The new() function has two arguments as string name and uvm_component parent.
 -Implement build_phase to create instances of environment, sequence classes, and set configurable objects in the configurable database.
 -Implement run_phase to start sequences on required sequencers with raise/drop objection callbacks.  
-
-//uvm_test Example :Generic Code Snippet
-
+  
+/////////////////////////////////////////////////
+   uvm_test Example :Generic Code Snippet
+////////////////////////////////////////////////
 class my_test extends uvm_test;
   env env_o;
   base_seq bseq;
@@ -48,12 +53,16 @@ class my_test extends uvm_test;
   endtask
 endclass  :my_test
 
-Execution of the test :
+////////////////////////////  
+  Execution of the test 
+/////////////////////////////  
 -The test execution is a time-consuming activity that runs sequence or sequences for DUT functionality. 
 -On executing the test, it builds a complete UVM testbench structure in the build_phase and time consuming activities are performed in the run_phase with the help of sequences. 
 -It is mandatory to register tests in the UVM factory otherwise, the simulation will terminate with UVM_FATAL (test not found).
 
-run_test() task :
+////////////////////  
+  run_test() task 
+////////////////////  
 -The run_test task is a global task declared in the uvm_root class and responsible for running a test case.  
 -Declaration:  virtual task run_test (string test_name = "")
 1. The run_test task starts the phasing mechanism that executes phases in a pre-defined order.
@@ -75,21 +84,6 @@ end
  +UVM_TESTNAME = my_test
 4. After execution of all phases, run_test finally calls $finish task for simulator exit.  
   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
   
