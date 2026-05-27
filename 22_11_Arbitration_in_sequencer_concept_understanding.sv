c5f51b2906620eb4ebcbb2ada2124f34b81a5513
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2026-05-27 19:51:43 +0530

    Update 22_11_Arbitration_in_sequencer_concept_understanding.sv

--- 22_11_Arbitration_in_sequencer_concept_understanding.sv
+++ 22_11_Arbitration_in_sequencer_concept_understanding.sv
@@ -1,16 +1,22 @@
-Arbitration in Sequencer Understanding:
+///////////////////////////////////////////////
+  Arbitration in Sequencer Understanding
+////////////////////////////////////////////////
 -The uvm_sequencer has a built-in mechanism to arbitrate within concurrently running sequences over the sequencer. 
 -Based on the arbitration algorithm, the sequencer sends sequence_item to the driver for the granted sequence.
 -Each sequence sends sequence_items also do have its own id that differentiates from other sequences.
 -To set a particular arbitration mechanism, the set_arbitration function is used and it is defined in the uvm_sequencer_base class.
-
-set_arbitration method :
+ 
+//////////////////////////////////
+  set_arbitration method 
+/////////////////////////////////
 -It is used to specify the arbitration mode or mechanism for the sequencer.
 -Decleration : function void set_arbitration( UVM_SEQ_ARB_TYPE val)
 -Any one of the below arbitration modes or algorithms can be selected.
 -For example: <sequencer_name>.set_arbitration(UVM_SEQ_ARB_RANDOM);
 
- Different Arbitration algorithms in uvm_sequencer :
+ //////////////////////////////////////////////////////////////
+   Different Arbitration algorithms in uvm_sequencer 
+ ///////////////////////////////////////////////////////////////
 
  Algorithms                                              Description
 
@@ -34,9 +40,10 @@ user_priority_arbitration :
 -The sequencer will call the user_priority_arbitration function when arbitration mode is set to UVM_SEQ_ARB_USER using the set_arbitration function. 
 -The user-defined sequencer can override the user_priority_arbitration method to implement a customized arbitration policy.
 -Decleration :  virtual function integer user_priority_arbitration(integer avail_sequences[$]) 
-
-Examples to understand Sequencer Arbitration Mode:
  
+///////////////////////////////////////////////////////
+  Examples to understand Sequencer Arbitration Mode
+////////////////////////////////////////////////////// 
  1. Different arbitration mode execution (Without any priority to the sequencer - Controlled in my_base_test)
      Link: https://github.com/Sibakumarpanda/UVM_Concepts_and_Coding_by_Siba/blob/main/22_Arbitration_in_sequencer_ex1_without_any_priority.sv
  
